feeffa0699a3f636406b313daebf60a0a4337046
hongzhu zhao <hongzhu.zhao@kangdainfo.com>
2024-02-27 10:35:35 +0800

    新增header格式 和 router

---
+++ header.js
@@ -0,0 +1,6 @@
+const header = {
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Method': 'GET, POST, PATCH, PUT, DELETE, OPTION',
+    'Content-Type': 'application/json'
+}
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const http = require('http')
+const header = require('./header')
 
 const server = http.createServer((req, res) => {
+    console.log('url',req.url)
+    console.log('method',req.method)    
 
+    if(req.url === '/') {
+        console.log('/')
+    } else if(req.url.includes('/todo')) {
+        console.log('/todo')
+    } else {
+        console.log('路由錯誤')
+    }
 })
 
 server.listen(3535, () => console.log('伺服器已啟動'))
